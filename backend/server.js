b3902eca6c8e23a3e354b96459f9a550d0e78775
SKSaranya-git <kirupananthansaranya@gmail.com>
2026-08-21 12:06:44 +0530

    Register wellness activity routes

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const journalRoutes = require("./src/routes/journalRoutes");
 const forumRoutes = require("./src/routes/forumRoutes");
 const peerSupporterRoutes = require("./src/routes/peerSupporterRoutes");
 const resourceRoutes = require("./src/routes/resourceRoutes");
+const wellnessActivityRoutes = require("./src/routes/wellnessActivityRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -26,6 +27,7 @@ app.use("/api/journals", journalRoutes);
 app.use("/api/forum", forumRoutes);
 app.use("/api/peer-supporters", peerSupporterRoutes);
 app.use("/api/resources", resourceRoutes);
+app.use("/api/wellness-activities", wellnessActivityRoutes);
 
 
 // Base route for health check
